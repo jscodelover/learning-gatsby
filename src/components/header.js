a010aadfe5aa1ae82af83447934755d08ffaafc5
jscodelover <manisha.4918@gmail.com>
2019-05-30 23:38:07 +0530

    sass plugin in gatsby

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import "./header.scss"
 
 function Header() {
   return (
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Header from "./header"
 import Footer from "./footer"
+import "../style/index.scss"
 
 function Layout(props) {
   return (
